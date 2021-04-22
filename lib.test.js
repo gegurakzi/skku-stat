6ffe7e80333cd8371d9486ada7eb10a8add7ebb2
gegurakzi <79631890+gegurakzi@users.noreply.github.com>
2021-04-22 16:09:59 +0900

    final

--- lib.test.js
+++ lib.test.js
@@ -19,3 +19,15 @@ test("med([5, 1, 2, 3, 3]) should be 3", () => {
 test("med([1, 2, 4, 4]) should be 3", () => {
     expect(lib.med([1, 2, 4, 4])).toBe(3);
 });
+test("sum([1@2, 3, 4]) should be 9", () => {
+    expect(lib.sum([1, 1, 3, 4])).toBe(9);
+});
+test("sum([2@3, -2, 1@3]) should be 7", () => {
+    expect(lib.sum([2, 2, 2, -2, 1, 1, 1])).toBe(7);
+});
+test("min([3@3, 5]) should be 3", () => {
+    expect(lib.med([3, 3, 3, 5])).toBe(3);
+});
+test("med([2@4, 1, 4@2]) should be 2", () => {
+    expect(lib.med([2, 2, 2, 2, 1, 4, 4])).toBe(2);
+});
--- main.js
+++ main.js
@@ -9,14 +9,39 @@ if (process.argv.length <= 3) {
 
 let command = process.argv[2];
 
+
+
 let numbers = process.argv
-    .slice(3, process.argv.length)
-    .map((n) => parseFloat(n));
+.slice(3, process.argv.length);
+
+for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i].indexOf("@") != -1) {
+        let nums = [];
+        let num = numbers[i].split("@")[0];
+        let iter = numbers[i].split("@")[1];
+    
+        if(iter <= 0 || num == "" || iter == ""){ 
+            console.log("Invalid repeat!");
+            process.exit(1);
+        }
+    
+        for (let i = 0; i < parseInt(iter); i++) {
+            nums = nums.concat(num) 
+        };
+        numbers = numbers.slice(0, i)
+        .concat(nums)
+        .concat(numbers.slice(i+1));
+    }
+}
+numbers = numbers.map((n) => parseFloat(n));
+
 
 if (numbers.some((n) => isNaN(n))) {
     console.log("Some arguments are not numbers!");
     process.exit(1);
 }
+
+
 
 let result;
 switch (command) {
